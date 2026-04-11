c8ae9f4242f4195a2c07c174f7bc76a19d7f187e
Lexter Hiteroza <ashketchopstick@gmail.com>
2026-04-11 12:51:35 +0800

    header done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 import odinImage from "./image.png";
 
-console.log(greeting);
-const image = document.createElement("img");
-image.src = odinImage;
-
-document.body.appendChild(image);
